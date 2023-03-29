643697ba16530bbeeb03e2212a93f2479db7eaee
unknown <earao@hotmail.com>
2023-03-29 13:45:23 -0700

    troubleshooting error in myApi.js

--- misc/apiTest/myApi.js
+++ misc/apiTest/myApi.js
@@ -2,14 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
+console.log("Body Parser Setup");
 
 const port = 5500;
 app.listen(port, () => {
     console.log("webserver listening on port " + port);
 });
+
+console.log("Port Opened");
 
 app.get("/testApi", async function (req, resp){
     resp.header("Access-Control-Allow-Origin", "*");
